31f9c6b45fe97d2af7353f9d48e7d0f71929bec6
robneu <rob@wpsitecare.com>
2015-12-24 01:51:19 -0500

    Added cssnano and pixrem PostCSS modules

--- config/grunt/postcss.js
+++ config/grunt/postcss.js
@@ -1,9 +1,13 @@
 // https://github.com/nDmitry/grunt-postcss
-var autoprefixer = require('autoprefixer');
+var autoprefixer = require( 'autoprefixer' ),
+	cssnano      = require( 'cssnano' ),
+	pixrem       = require( 'pixrem' );
 
 module.exports = {
 	options: {
+		map: true,
 		processors: [
+			pixrem,
 			autoprefixer({
 				browsers: [
 					'Android >= 2.1',
@@ -13,12 +17,23 @@ module.exports = {
 					'Opera >= 12.1',
 					'Safari >= 6.0'
 				]
-			})
-		],
-		map: true
+			}),
+			cssnano
+		]
 	},
 	theme: {
 		src: '<%= paths.tmp %>style.css',
 		dest: '<%= paths.tmp %>style.css'
+	},
+	vendor: {
+		expand: true,
+		cwd: 'css/',
+		src: [
+			'*.css',
+			'!*.min.css'
+		],
+		dest: 'css/',
+		ext: '.min.css',
+		extDot: 'last'
 	}
 };
