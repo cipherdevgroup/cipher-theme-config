8cf9224ad6ad948902274a7798a44371dac10ba7
robneu <rob@wpsitecare.com>
2015-12-24 03:22:12 -0500

    Revert "Added cssnano and pixrem PostCSS modules"
    This reverts commit 31f9c6b45fe97d2af7353f9d48e7d0f71929bec6.

--- config/grunt/postcss.js
+++ config/grunt/postcss.js
@@ -1,13 +1,9 @@
 // https://github.com/nDmitry/grunt-postcss
-var autoprefixer = require( 'autoprefixer' ),
-	cssnano      = require( 'cssnano' ),
-	pixrem       = require( 'pixrem' );
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
 	options: {
-		map: true,
 		processors: [
-			pixrem,
 			autoprefixer({
 				browsers: [
 					'Android >= 2.1',
@@ -17,23 +13,12 @@ module.exports = {
 					'Opera >= 12.1',
 					'Safari >= 6.0'
 				]
-			}),
-			cssnano
-		]
+			})
+		],
+		map: true
 	},
 	theme: {
 		src: '<%= paths.tmp %>style.css',
 		dest: '<%= paths.tmp %>style.css'
-	},
-	vendor: {
-		expand: true,
-		cwd: 'css/',
-		src: [
-			'*.css',
-			'!*.min.css'
-		],
-		dest: 'css/',
-		ext: '.min.css',
-		extDot: 'last'
 	}
 };
