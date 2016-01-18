063f5fb406b1448f3a61de82e7fc9745302730d3
robneu <rob@wpsitecare.com>
2016-01-18 17:47:43 -0500

    Adjusted JS path again
    This should only match files within the assets/theme/js dir now

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ module.exports = function() {
 
 	project.taskMap = {
 		addtextdomain: 'grunt-wp-i18n',
-		readpkg:       'sitecare-theme-config',
-		makepot:       'grunt-wp-i18n'
+		makepot:       'grunt-wp-i18n',
+		readpkg:       'sitecare-theme-config'
 	};
 
 	project.files = {
-		js:     project.paths.assets       + '{,*/}js/*.js',
+		js:     project.paths.authorAssets + 'js/**/*.js',
 		scss:   project.paths.authorAssets + 'scss/**/*.scss',
 		config: project.paths.config       + '**/*.js',
 		changelog: 'CHANGELOG.md'
