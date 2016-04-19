ab9675d3d91a034ae9eb283441cdbfafec94ba1b
robneu <rob@wpsitecare.com>
2016-04-19 14:44:52 -0400

    Pulled in theme-specific tasks
    Needs override testing

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -31,7 +31,59 @@ module.exports = function() {
 			'build:dependencies:js',
 			'build:dependencies:php'
 		],
-
+		'build:dependencies:fonts': [
+			'clean:fonts',
+			'shell:bower',
+			'copy:bowerfonts'
+		],
+		'build:dependencies:css': [
+			'clean:css',
+			'shell:bower',
+			'copy:bowercss'
+		],
+		'build:dependencies:js': [
+			'clean:js',
+			'shell:bower',
+			'copy:bowerjs'
+		],
+		'build:dependencies:php': [
+			'clean:php',
+			'shell:composer',
+			'newer:copy:php'
+		],
+		'build:fonts': [
+			'build:dependencies:fonts',
+			'newer:copy:fonts'
+		],
+		'build:css': [
+			'build:dependencies:css',
+			'newer:sass',
+			'newer:postcss',
+			'newer:rtlcss',
+			'newer:cssmin',
+			'newer:copy:css'
+		],
+		'build:css:dist': [
+			'build:dependencies:css',
+			'newer:sass',
+			'newer:postcss',
+			'newer:rtlcss',
+			'newer:wpcss',
+			'newer:copy:css'
+		],
+		'build:images': [
+			'newer:imagemin:images',
+			'newer:copy:images'
+		],
+		'build:js': [
+			'build:dependencies:js',
+			'newer:concat:js',
+			'newer:uglify'
+		],
+		'build:js:dist': [
+			'build:dependencies:js',
+			'newer:concat:js'
+		],
 		'build:i18n': [
 			'clean:languages',
 			'addtextdomain',
