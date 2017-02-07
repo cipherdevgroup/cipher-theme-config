93ca663aea6d933be8426dc6aa825109d0ff2de9
robneu <rob@wpsitecare.com>
2017-02-07 16:01:11 -0500

    Cloned in Cedaro's config

--- config/grunt/makepot.js
+++ config/grunt/makepot.js
@@ -1,17 +1,36 @@
-// https://github.com/blazersix/grunt-wp-i18n
+// https://github.com/cedaro/grunt-wp-i18n
 module.exports = {
 	theme: {
 		options: {
 			domainPath: '<%= paths.languages %>',
-			updateTimestamp: false,
 			exclude: [
 				'<%= paths.composer %>.*'
 			],
 			potHeaders: {
-				poedit: true,
-				'report-msgid-bugs-to': '<%= package.pot.reportmsgidbugsto %>',
-				'last-translator': '<%= package.pot.lasttranslator %>',
-				'language-team': '<%= package.pot.languageteam %>'
+				'language': 'en',
+				'plural-forms': 'nplurals=2; plural=(n != 1);',
+				'poedit': true,
+				'report-msgid-bugs-to': 'https://www.wpsitecare.com/contact/'
+			},
+			processPot: function( pot ) {
+				var translation,
+					excludedMeta = [
+						'Theme Name of the plugin/theme',
+						'Theme URI of the plugin/theme',
+						'Author of the plugin/theme',
+						'Author URI of the plugin/theme'
+					];
+
+				for ( translation in pot.translations[''] ) {
+					if ( 'undefined' !== typeof pot.translations[''][ translation ].comments.extracted ) {
+						if ( 0 <= excludedMeta.indexOf( pot.translations[''][ translation ].comments.extracted ) ) {
+							console.log( 'Excluded meta: ' + pot.translations[''][ translation ].comments.extracted );
+							delete pot.translations[''][ translation ];
+						}
+					}
+				}
+
+				return pot;
 			},
 			type: 'wp-theme'
 		}
